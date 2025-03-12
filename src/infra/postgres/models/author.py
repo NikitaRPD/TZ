bc2e6bb197fbcad9df39f3fc892e892fdcff38d6
CherryNick <dev.nikita@innofin.team>
2025-03-12 12:28:03 +0300

    rev: v3

--- src/infra/postgres/models/author.py
+++ src/infra/postgres/models/author.py
@@ -1,6 +1,6 @@
-from uuid import UUID, uuid4
+from uuid import UUID
 
-from sqlalchemy import UUID as PGUUID
+from sqlalchemy import UUID as PGUUID, text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.infra.postgres.models.base import Base
@@ -9,5 +9,9 @@ from src.infra.postgres.models.base import Base
 class Author(Base):
     __tablename__ = 'authors'
 
-    id: Mapped[UUID] = mapped_column(PGUUID(as_uuid=True), primary_key=True, default=uuid4)
+    id: Mapped[UUID] = mapped_column(
+        PGUUID(as_uuid=True),
+        primary_key=True,
+        server_default=text("gen_random_uuid()")
+    )
     name: Mapped[str]
